19b5cda0eba73cad5c3b126809b1d5c3e8460fa2
Ahmad91-9 <malikahmad7awan3434@gmail.com>
2025-11-18 19:06:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ GITHUB_APPS_DICT = [
         "title": "Archive.org Downloader",
         "icon": iconarh,
         "type": "pyside6_gui",
-        "entry": "https://raw.githubusercontent.com/Ahmad91-9/fsap/main/archivedownloader.py",
+        "entry": "src/src/archivedownloader.py",
         "version": "0.0.0",
         "description": "Local downloader for archives",
     },
@@ -251,5 +251,6 @@ def weekly_reward_on_a_successful_referral():
 
 
     return 20
+
 
 
